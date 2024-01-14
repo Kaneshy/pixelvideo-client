c22442947158625c390c386a3f4229662a323022
david <kaneshiuchiha@gmail.com>
2024-01-14 13:37:53 -0500

    infinite scroll

--- app/Home/page.jsx
+++ app/Home/page.jsx
@@ -6,44 +6,66 @@ import Card from '@/components/Card'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import TrendTopbar from '@/components/bars/TrendTopbar'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 
 const HomePage = () => {
 
   const [videos, setVideos] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [page, setPage] = useState(1);
+
+  const fetchVideos = async () => {
+    console.log('gs', page)
+    try {
+      const res = await axios.get(`/api/videos/random?page=${page}`);
+      const data = await res.data;
+
+
+      if (data.length === 0) {
+        setHasMore(false);
+      } else {
+        setVideos((prevPosts) => [...prevPosts, ...data]);
+        setPage((prevPage) => prevPage + 1);
+      }
+
+
+    } catch (error) {
+      console.error('Error fetching videos:', error);
+    }
+  };
 
   useEffect(() => {
-    const fetchVideos = async () => {
-      try {
-        const res = await axios.get(`/api/videos/random`);
-        setVideos(res.data);
-        console.log(res.data)
-      } catch (error) {
-        console.error('Error fetching videos:', error);
-      }
-    };
+    console.log('run')
     fetchVideos();
   }, []);
 
 
   return (
 
-    <div id='a' className=' gap-y-5 p-4  flex flex-col items-center max-sm:p-0'>
-      <TrendTopbar />
-      <div className='home-sv-a'>
-        {videos.map((video, index) => {
-          return (
-            <Link href={`/Video/${video._id}`} key={video._id}>
-              <div className='rounded-2xl select-none relative bg-opacity-40 flex flex-col p-2 gap-y-7   shadow bg-neutral-900 max-sm:p-0 '>
+    <InfiniteScroll
+      dataLength={videos.length}
+      next={fetchVideos}
+      hasMore={hasMore}
+      loader={<h4>Loading...</h4>}
+    >
+      <div id='a' className=' gap-y-5 p-4  flex flex-col items-center max-sm:p-0'>
+        <TrendTopbar />
+        <div className='home-sv-a'>
+          {videos.map((video, index) => {
+            return (
+              <Link href={`/Video/${video._id}`} key={video._id+index}>
+                <div className='rounded-2xl select-none relative bg-opacity-40 flex flex-col p-2 gap-y-7   shadow bg-neutral-900 max-sm:p-0 '>
 
-                <Card video={video} />
+                  <Card video={video} />
 
-              </div>
-            </Link>
-          )
-        })}
+                </div>
+              </Link>
+            )
+          })}
+        </div>
       </div>
-    </div>
+    </InfiniteScroll>
   )
 }
 
--- app/api/videos/random/route.js
+++ app/api/videos/random/route.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(req, {params}) {
+  const page = Number(req.url.split('?page=')[1])
+  console.log(page)
+  const searchParams = new URLSearchParams(req.url);
+  console.log('hhh', searchParams)
+  // const page = searchParams.get('page') 
+
   try {
-    const response = await axios.get('https://pixelvideo.up.railway.app/api/videos/random');
+    const response = await axios.get(`https://pixelvideo.up.railway.app/api/videos/random?page=${page}`);
     const responseData = response.data; // Extract necessary data from the response
 
     return NextResponse.json(responseData); // Return only the necessary data
@@ -12,3 +18,17 @@ export async function GET() {
     return NextResponse.error();
   }
 }
+
+// import { connectToDatabase } from '../../db';
+
+// export default async function handler(req, res) {
+//   const { page = 1, pageSize = 10 } = req.query;
+
+//   const database = await connectToDatabase();
+//   const collection = database.collection('posts');
+
+//   const skip = (page - 1) * pageSize;
+//   const posts = await collection.find().skip(skip).limit(pageSize).toArray();
+
+//   res.status(200).json(posts);
+// }
